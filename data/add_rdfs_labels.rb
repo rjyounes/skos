b4783b812e48a98aed6a46e3360633a7d9bfe08d
Rebecca Younes <rjy7@lib-c02mjedqfd57.library.cornell.edu>
2015-08-05 11:44:52 -0400

    Add rdfs ns declaration to file

--- data/add_rdfs_labels.rb
+++ data/add_rdfs_labels.rb
@@ -7,10 +7,10 @@ infile = ARGV[0]
 bak = File.basename(infile, ".*") + ".bak.rdf"
 
 s = ''
-File.open(infile) do |f|
-  s = f.read
-  open(bak, 'w') { |b| b.puts s }
-  s.gsub!(/((^\s*)<skos:prefLabel([^>]+)>(.+)<\/skos:prefLabel>)/, "\\1\n\\2<rdfs:label\\3>\\4</rdfs:label>")
-end
+File.open(infile) { |f| s = f.read }
+File.open(bak, 'w') { |b| b.puts s }
+rdfsns = 'xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"'
+s.sub!(/(<rdf:RDF)/, "\\1\n    #{rdfsns}") unless s.include? rdfsns
+s.gsub!(/((^\s*)<skos:prefLabel([^>]+)>(.+)<\/skos:prefLabel>)/, "\\1\n\\2<rdfs:label\\3>\\4</rdfs:label>")
 File.open(infile, 'w') { |f| f.puts s}
 
